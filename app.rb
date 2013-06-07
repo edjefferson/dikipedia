a55c54b6b8e1c8a1512b1a326946a000c5f8a561
Ed Jefferson <e.jefferson@gmail.com>
2013-06-07 14:54:43 +0100

    dedupe attempt 1

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require "sinatra"
 require "active_record"
 require "mysql2"
+require "twitter"
 
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
@@ -11,15 +12,24 @@ ActiveRecord::Base.establish_connection(
   :database => ENV['DBNAME']
 )
 
+Twitter = Twitter.configure do |config|
+    config.consumer_key = ENV['YOUR_CONSUMER_KEY']
+    config.consumer_secret = ENV['YOUR_CONSUMER_SECRET']
+    config.oauth_token = ENV['YOUR_OAUTH_TOKEN']
+    config.oauth_token_secret = ENV['YOUR_OAUTH_TOKEN_SECRET']
+  end
+
 class Topic < ActiveRecord::Base
 end
 
 
 
 get '/topic/:text' do
-	url = params[:text].to_s
+	url = params[:text].to_s.split("/")
+	puts url[-1]
 	if (Topic.count(:conditions => "topic = '#{url}' AND date(viewed_at) = date('#{Time.now}')") == 0)
 
   Topic.create(:topic=> url, :viewed_at => Time.now)
+  Twitter.update(url)
 end
 end
