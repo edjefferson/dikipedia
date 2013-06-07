0ab08b7c58db4382062cf4adeddd1a5181bbd33e
Ed Jefferson <e.jefferson@gmail.com>
2013-06-07 14:58:17 +0100

    dedupe attempt 1

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ require "active_record"
 require "mysql2"
 require "twitter"
 
+class String
+  def trim140
+    tweet = self
+    while tweet.length>140
+      tweetwords=tweet.split(' ')
+      tweet=tweetwords[0..-2].join(' ') << "..."
+ 
+    end
+    return tweet
+  end
+
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
   :host     => ENV['DBADDRESS'],
@@ -28,9 +39,9 @@ end
 get '/topic/:text' do
 	url = params[:text].to_s.split("/")
 	puts url[-1]
-	if (Topic.count(:conditions => "topic = '#{url}' AND date(viewed_at) = date('#{Time.now}')") == 0)
+	if (Topic.count(:conditions => "topic = '#{url[-1]}' AND date(viewed_at) = date('#{Time.now}')") == 0)
 
-  Topic.create(:topic=> url, :viewed_at => Time.now)
+  Topic.create(:topic=> url[-1], :viewed_at => Time.now)
   #Twitter.update(url)
 end
 end
